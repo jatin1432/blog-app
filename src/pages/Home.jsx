5c815f9d84d61610df76651cf15bd707ffe738d3
Jatin Sharma <115401137+jatin1432@users.noreply.github.com>
2026-03-05 18:59:21 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ function Home({ user }) {
   const fetchBlogs = async () => {
     try {
       const res = await axios.get(
-        `https://blog-app.rf.gd/api/posts/index.php`,
+        `http://blog-app.rf.gd/api/posts/index.php`,
         {
           params: {
             page,
